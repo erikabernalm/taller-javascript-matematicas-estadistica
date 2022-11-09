814d41cddba7d94aff5fbaf71545eb0690af0475
Erika Bernal <bmnathis@gmail.com>
2022-11-09 17:23:46 -0500

    Cupones de descuento

--- descuentos.js
+++ descuentos.js
@@ -1,5 +1,6 @@
 const inputPrice = document.querySelector('#price');
-const inputDiscount = document.querySelector('#discount'); // Selectores
+// const inputDiscount = document.querySelector('#discount'); // Selectores
+const inputCoupon = document.querySelector('#coupon');
 const btn = document.querySelector('#calcular');
 const pResult = document.querySelector('#result');
 
@@ -7,21 +8,52 @@ btn.addEventListener('click', calcularPrecioConDescuento); // AQUÍ estamos escu
 
 function calcularPrecioConDescuento() {
 	const price = Number(inputPrice.value);
-	const discount = Number(inputDiscount.value);
+	// const discount = Number(inputDiscount.value);
+	const coupon = inputCoupon.value;
+	
+	if (!price || !coupon) {
+		pResult.innerText = 'Por favor llena el formulario';
+		return;
+	}
+	
+	let discount; 
+
+	switch (coupon) {
+		case 'Gold':
+			discount = 30;
+			break;
+		case 'Silver':
+			discount = 20;
+			break;
+		default:
+			pResult.innerText = 'El cupón no es válido';
+		  return;
+	}
+
+	// Otra forma de hacerlo
+	// if (coupon == 'Gold') {
+	// 	discount = 30;
+	// } else if (coupon == 'Silver') {
+	// 	discount = 20;
+	// } else {
+	// 	pResult.innerText = 'El cupón no es válido';
+	// 	return;
+	// }
+
 
 	// console.log({ price, discount });
 
-	if (!price || !discount) {
-		pResult.innerHTML = 'Por favor llena el formulario';
-		return;
-	}
+	// if (!price || !discount) {
+	// 	pResult.innerText = 'Por favor llena el formulario';
+	// 	return;
+	// }
 
-	if (discount > 100) {
-		pResult.innerHTML = 'Tal descuento NO existe, por favor paga!';
-		return;
-	}
+	// if (discount > 100) {
+	// 	pResult.innerText = 'Tal descuento NO existe, por favor paga!';
+	// 	return;
+	// }
 
 	const newPrice = (price * (100 - discount)) / 100;
 
-	pResult.innerHTML = 'El nuevo precio con descuento es $' + newPrice;
+	pResult.innerText = 'El nuevo precio con descuento es $' + newPrice;
 }
