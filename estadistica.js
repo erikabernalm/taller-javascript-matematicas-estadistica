4f6d08a9d4158d5f64549ab4efe1b5eeb3bb2daa
Erika Bernal <bmnathis@gmail.com>
2022-11-14 12:32:09 -0500

    Método reduce() y sintaxis de las Arrow function () => {}

--- estadistica.js
+++ estadistica.js
@@ -1,14 +1,48 @@
 // Calculando el promedio
-function calcularPromedio(lista) {
-	let sumaLista = 0; // contador
+// function calcularPromedio(lista) {
+// 	let sumaLista = 0; // contador
 
-	for (let i = 0; i < lista.length; i++) {
-		sumaLista = sumaLista + lista[i];
-	}
+// 	for (let i = 0; i < lista.length; i++) {
+// 		sumaLista = sumaLista + lista[i];
+// 	}
+
+// 	const promedio = sumaLista / lista.length;
+// 	console.log(promedio);
+// 	return promedio;
+// }
+
+// calcularPromedio([1,2,3,4,5]);
+
+
+// Método reduce
+
+function calcularPromedio(lista) {
+	// Primer forma de hacerlo
+	// let sumaLista = 0; // contador
+
+	// for (let i = 0; i < lista.length; i++) {
+	// 	sumaLista = sumaLista + lista[i];
+	// }
+
+	// Segunda forma
+	// function sumarTodosLosElementos(valorAcumulado, nuevoValor) {
+	// 	return valorAcumulado + nuevoValor;
+	// }
+
+	// const sumaLista = lista.reduce(sumarTodosLosElementos);
+
+	// Tercer forma. Uso de la sintaxis de las Arrow function () => {}
+
+	// const sumarTodosLosElementos = (valorAcumulado, nuevoValor) => {
+	// 	return valorAcumulado + nuevoValor;
+	// };
+	
+	// Reducción de líneas de código
+	const sumarTodosLosElementos = (valorAcumulado, nuevoValor) => valorAcumulado + nuevoValor; // Implícitamente va a retornar algo
+
+	const sumaLista = lista.reduce(sumarTodosLosElementos);
 
 	const promedio = sumaLista / lista.length;
 	console.log(promedio);
 	return promedio;
 }
-
-// calcularPromedio([1,2,3,4,5]);
