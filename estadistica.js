ceb6fdee554f1a014d10e47c6e57a6072d872355
Erika Bernal <bmnathis@gmail.com>
2022-11-17 14:10:39 -0500

    Calculando la moda: objetos a partir de arrays

--- estadistica.js
+++ estadistica.js
@@ -149,3 +149,21 @@ function ordenarLista(listaDesordenada) {
 
 	return lista;
 }
+
+// Calculando la moda: objetos a partir de arrays
+
+function calcularModa(lista) {
+	const listaCount = {};
+
+	for (let i = 0; i < lista.length; i++) {
+		const elemento = lista[i];
+
+		if (listaCount[elemento]) {
+			listaCount[elemento] += 1;
+		} else {
+			listaCount[elemento] = 1;
+		}
+	}
+
+	console.log(listaCount);
+}
