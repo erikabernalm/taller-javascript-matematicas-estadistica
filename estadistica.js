e79c657cc68ca0ccb565f698edfe52474a4efff6
Erika Bernal <bmnathis@gmail.com>
2022-11-22 11:13:58 -0500

    Calculando el promedio ponderado usando los métodos map y reduce de los arrays

--- estadistica.js
+++ estadistica.js
@@ -239,3 +239,54 @@ function solution(obj) {
 }
 
 solution(obj);
+
+// Calculando el promedio ponderado
+
+const notes = [
+  {
+    course: "Educación Física",
+    note: 10,
+    credit: 2
+  },
+  {
+    course: "Programación",
+    note: 8,
+    credit: 5
+  },
+  {
+    course: "Finanzas personales",
+    note: 7,
+    credit: 5
+  }
+];
+
+const notesWithCredit = notes.map(function (noteObject) {
+	return noteObject.note * noteObject.credit;
+});
+
+const sumOfNotesWithCredit = notesWithCredit.reduce(
+	function (sum = 0, newVal) {
+		return sum + newVal;
+	}
+);
+
+const credits = notes.map(function (noteObject) {
+	return noteObject.credit;
+});
+
+const sumOfCredits = credits.reduce(
+	function (sum = 0, newVal) {
+		return sum + newVal;
+	}
+);
+
+const promedioPonderadoNotasConCreditos = sumOfNotesWithCredit / sumOfCredits;
+
+console.log({
+	notes,
+	notesWithCredit,
+	sumOfNotesWithCredit,
+	credits,
+	sumOfCredits,
+	promedioPonderadoNotasConCreditos
+});
