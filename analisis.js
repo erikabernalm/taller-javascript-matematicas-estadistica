bb1215c7c3c3feb39db125b30e6f8c87beaa4ef5
Erika Bernal <bmnathis@gmail.com>
2022-11-28 09:45:17 -0500

    Proyección salarial personal

--- analisis.js
+++ analisis.js
@@ -37,3 +37,28 @@ function medianaPorPersona(nombrePersona) {
 medianaPorPersona('Juanita') // 950
 medianaPorPersona('Daniela') // 525
 console.log(medianaPorPersona('Daniela')); // 525
+
+// Proyección salarial personal
+
+function proyeccionPorPersona(nombrePersona) {
+	const trabajos = encontrarPersona(nombrePersona).trabajos;
+
+	let porcentajesCrecimiento = []; // Contador y/o objetivo
+
+	for (let i = 1; i < trabajos.length; i++) {
+		const salarioActual = trabajos[i].salario;
+		const salarioAnterior = trabajos[i - 1].salario;
+		const crecimiento = salarioActual - salarioAnterior;
+		const porcentajeCrecimiento = crecimiento / salarioAnterior;
+		porcentajesCrecimiento.push(porcentajeCrecimiento);
+		// console.log({salarioActual, salarioAnterior, crecimiento, porcentajeCrecimiento, porcentajesCrecimiento});
+	}
+	
+	const medianaPorcentajesCrecimiento = Estadistica.calcularMediana(porcentajesCrecimiento);
+	const ultimoSalario = trabajos[trabajos.length - 1].salario;
+	const aumento = ultimoSalario * medianaPorcentajesCrecimiento;
+	const nuevoSalario = ultimoSalario + aumento;
+	
+	// console.log({trabajos, porcentajesCrecimiento, medianaPorcentajesCrecimiento, ultimoSalario, aumento, nuevoSalario});
+	return nuevoSalario;
+}
